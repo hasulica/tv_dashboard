312636a99ed9a2c9e0d9ef9ca3be0027a2e43ba3
Radu Asandei <rasandei@deloitte.co.uk>
2016-10-21 12:07:36 +0100

    Wunderlist functionality

--- jobs/wunderlist.rb
+++ jobs/wunderlist.rb
@@ -1,25 +1,17 @@
 require 'wunderlist'
 
-wl = Wunderlist::API.new({
-  :access_token => 'aef0f1dd1e0da89cc2cc7cc7836c92ee7c3e9a0cb058ee2d030c8b94aba8',
-  :client_id => 'ea56d7159fc0333cc2aa'
-})
-
-puts wl
+wl = Wunderlist::API.new(access_token: 'aef0f1dd1e0da89cc2cc7cc7836c92ee7c3e9a0cb058ee2d030c8b94aba8',
+                         client_id: 'ea56d7159fc0333cc2aa')
 
 list = wl.list('inbox')
 
-puts list.tasks[0].title
-
-# SCHEDULER.every '10m', :first_in => do |job|
-# 	begin
-# 		list = wl.list('Inbox')
-#
-#     if list
-#       list = list.map do |item|
-#         { name: item.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
-#       end
-#       send_event('twitter_mentions', comments: tweets)
-#     end
-#   end
-# end
+SCHEDULER.every '10m', first_in: 0 do |_job|
+	begin
+		if list.tasks
+			list = list.tasks.map{ |item|
+				{ value: item.title }
+			}
+			send_event('wunderlist', items: list)
+		end
+	end
+end
